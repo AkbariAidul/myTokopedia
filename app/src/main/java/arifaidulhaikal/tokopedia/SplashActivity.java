87614dc21744706031b981d6e11c4576b5fee423
Muhammad Haikal <muhammadhaikal3037@gmail.com>
2026-05-21 23:55:04 +0800

    feat(ui): menambahkan ikon peluncur aplikasi dan meningkatkan UI layar splash
    - Menambahkan ikon peluncur aplikasi kustom dengan beberapa varian kepadatan (mdpi, hdpi, xhdpi, xxhdpi, xxxhdpi)
    - Menambahkan ic_launcher_background.xml dan konfigurasi ikon adaptif anydpi-v26
    - Menambahkan aset drawable logo_toko.png dan bg_ijo.png untuk branding layar splash
    - Memperbarui AndroidManifest.xml untuk mereferensikan ikon peluncur dan mengaktifkan akselerasi perangkat keras
    - Memperbaiki SplashActivity untuk menggunakan navigasi yang dipicu tombol alih-alih penundaan otomatis
    - Menambahkan pencatatan dan penanganan kesalahan ke SplashActivity untuk debugging yang lebih baik
    - Memperbarui tata letak activity_splash.xml dengan elemen UI baru
    - Memperbarui konfigurasi tema di themes.xml
    - Memperbarui build.gradle dan gradle-wrapper.properties untuk konsistensi build

--- app/src/main/java/arifaidulhaikal/tokopedia/SplashActivity.java
+++ app/src/main/java/arifaidulhaikal/tokopedia/SplashActivity.java
@@ -2,22 +2,40 @@ package arifaidulhaikal.tokopedia;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    private static final String TAG = "SplashActivity";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
-                finish();
-            }
-        }, 3000);
+        
+        try {
+            Log.d(TAG, "Setting content view");
+            setContentView(R.layout.activity_splash);
+            Log.d(TAG, "Content view set successfully");
+            
+            // Inisialisasi tombol Mulai Belanja
+            Button btnMulai = findViewById(R.id.btn_mulai);
+            btnMulai.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        Log.d(TAG, "Button clicked, starting LoginActivity");
+                        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                        finish();
+                    } catch (Exception e) {
+                        Log.e(TAG, "Error starting LoginActivity", e);
+                    }
+                }
+            });
+            
+        } catch (Exception e) {
+            Log.e(TAG, "Error in onCreate", e);
+        }
     }
 }
